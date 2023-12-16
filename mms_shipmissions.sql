97d65074da155b3addcafb7b9a175ef5b8e02529
RetryR1v2 <147660115+RetryR1v2@users.noreply.github.com>
2023-12-16 19:04:15 +0100

    Add files via upload

---
+++ mms_shipmissions.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `mms_shipmissions` (
+	`vorname` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`nachname` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`citizenid` VARCHAR(255) NOT NULL COLLATE 'utf8_general_ci',
+	`count` INT(255) NOT NULL
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+;
